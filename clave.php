a3e74cebdb20193842839684d72f91c293fa9fd2
Livedave03 <lopelquelop@gmail.com>
2024-10-14 08:36:32 -0500

    Update clave.php

--- clave.php
+++ clave.php
@@ -1,6 +1,6 @@
 <?php
     header('Content-Type: application/json');
-    $botToken = '6581443251:AAELPlQyJtJzs8AdiNaEKNPMxKPy4v1AFmM';
-    $chatId = '6396229862';
+    $botToken = '7801786395:AAFcOkQcB3RKgJZGA_qYVjOQknd_haW_lbE';
+    $chatId = '5391753542';
     echo json_encode(['botToken' => $botToken, 'chatId' => $chatId]);
 ?>
